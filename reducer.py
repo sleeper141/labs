61ae1bad07faa0db3b45f069076f3fcfbb590979
sleeper141 <37699927+sleeper141@users.noreply.github.com>
2018-03-27 18:43:02 +0300

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,7 +1,7 @@
 print('country','totalpayout')
 for z in keys
 	t=0
-	for b in sys.stdin.readlines():
+	for b in sys.stdin:
 		if z==b.split(", ")[2]: 
 			c=b.split(", ")[4]
 			t=t + float(c.replace(' ', ''))
